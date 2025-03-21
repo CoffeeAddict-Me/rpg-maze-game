b84f10102920e6a7dc7a16bcf7492bf606f3fa74
Kasia K <kasia.dybo@gmail.com>
2025-03-21 22:20:20 +1000

    added dice, hud wip

--- src/app/components/dice/dice.component.ts
+++ src/app/components/dice/dice.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dice',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './dice.component.scss'
 })
 export class DiceComponent {
+  currentRoll: number = 1;
 
+  @Output() rolled = new EventEmitter<number>(); // Emit a number
+
+  rollDice() {
+    this.currentRoll = Math.floor(Math.random() * 6) + 1;
+    this.rolled.emit(this.currentRoll); // Emit the dice value
+  }
 }
--- src/app/components/hud/hud.component.ts
+++ src/app/components/hud/hud.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Unit } from '../../model/unit.model';
+import { GameService } from '../../services/game.service';
 
 @Component({
   selector: 'app-hud',
   templateUrl: './hud.component.html',
   styleUrl: './hud.component.scss'
 })
-export class HudComponent {
+export class HudComponent implements OnInit {
+  player: Unit | null | undefined;
+  inventory: string[] = ['Potion', 'Key', 'Scroll'];
+  log: string[] = ['Welcome to the Maze!', 'You found a potion!'];
 
+
+
+  constructor(private gameService: GameService) { }
+
+  ngOnInit(): void {
+    this.player = this.gameService.getPlayer();
+  }
+  
+  onDiceRolled(result: number) {
+    this.log.push(`You rolled a ${result}`);
+  }
 }
